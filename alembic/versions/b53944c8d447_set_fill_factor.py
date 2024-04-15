156c14352e0ee8e0952dd094ce85747836959447
lukas kotatko <lukas.kotatko@heureka.cz>
2024-04-15 13:45:18 +0200

    PPCD-1011 set fill-factor on tables

---
+++ alembic/versions/b53944c8d447_set_fill_factor.py
@@ -0,0 +1,26 @@
+"""Set fill factor
+
+Revision ID: b53944c8d447
+Revises: 735f11dd2cd8
+Create Date: 2024-04-15 11:39:41.989690
+
+"""
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "b53944c8d447"
+down_revision = "735f11dd2cd8"
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    op.execute("ALTER TABLE offers SET (FILLFACTOR = 70)")
+    op.execute("ALTER TABLE buyables SET (FILLFACTOR = 70)")
+    op.execute("ALTER TABLE availabilities SET (FILLFACTOR = 70)")
+
+
+def downgrade() -> None:
+    op.execute("ALTER TABLE offers RESET (FILLFACTOR)")
+    op.execute("ALTER TABLE buyables RESET (FILLFACTOR)")
+    op.execute("ALTER TABLE availabilities RESET (FILLFACTOR)")
